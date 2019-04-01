5ab1ba341cfefe6c274f60419341d00f2fa439db
Izzy <izzy@izzys-mbp.home>
2019-04-01 00:05:21 -0400

    Add material UI elements for Movie comp

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
-const Movie = () => {
-  return(
-    <div>
-      I am a movie.
-    </div>
-  )
+const Movie = (props) => {
+    console.log(props)
+    return(
+        <div>
+            { props.movie ? (
+                <Card >
+                    {/* <CardMedia style={{height: 0, paddingTop: '56.25%'}}
+                    image={props.movie.fields.movieImage.fields.file.url}
+                    title={props.movie.fields.title}
+                    /> */}
+                    <CardContent>
+                    <Typography gutterBottom variant="headline" component="h2">
+                        {props.movie.title}
+                    </Typography>
+                    <Typography component="p">
+                        {props.movie.release_date}
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small" color="primary" href={props.movie.url} target="_blank">
+                        Go To Movie
+                    </Button>
+                    </CardActions>
+                </Card>
+            ) : null}
+        </div>
+    )
 }
 
 export default Movie;
