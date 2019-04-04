e1921a3a6bb11864aac845c9b4381a616ae66d08
Izzy <izzy@izzys-mbp.home>
2019-04-04 00:55:34 -0400

    Add MoviesList comp w/ error & new props handling, Movies

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,33 +1,40 @@
-import React from 'react'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import titlePic from '../../assets/starWarsPlaceholder.jpg';
+import './Movie.css';
 
 const Movie = (props) => {
-    console.log(props)
+    console.log('movie props', props)
     return(
         <div>
             { props.movie ? (
                 <Card >
-                    {/* <CardMedia style={{height: 0, paddingTop: '56.25%'}}
-                    image={props.movie.fields.movieImage.fields.file.url}
-                    title={props.movie.fields.title}
-                    /> */}
+                    <CardMedia style={{height: 0, paddingTop: '56.25%'}}
+                    image={titlePic}
+                    title={props.movie.title}
+                    />
                     <CardContent>
-                    <Typography gutterBottom variant="headline" component="h2">
-                        {props.movie.title}
-                    </Typography>
-                    <Typography component="p">
-                        {props.movie.release_date}
-                    </Typography>
+                        <Typography gutterBottom variant="headline" component="h2">
+                            {props.movie.title}
+                        </Typography>
+                        <Typography component="p">
+                            {props.movie.release_date}
+                        </Typography>
                     </CardContent>
                     <CardActions>
-                    <Button size="small" color="primary" href={props.movie.url} target="_blank">
-                        Go To Movie
-                    </Button>
+                        <Button 
+                            size="small" 
+                            color="primary" 
+                            href={props.movie.url} 
+                            target="_blank"
+                        >
+                            Go To Movie
+                        </Button>
                     </CardActions>
                 </Card>
             ) : null}
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,82 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Movie from '../Movie/Movie';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import * as contentful from 'contentful'
-import Movie from '../components/Movie'
-
-const SPACE_ID = '[INSERT CONTENTFUL SPACE ID]'
-const ACCESS_TOKEN = '[INSERT CONTENTFUL ACCESS TOKEN]'
-const client = contentful.createClient({
-    space: SPACE_ID,
-    accessToken: ACCESS_TOKEN
-})
+import './MoviesList.css';
 
 class MoviesList extends Component {
 
-    state = {
-        movies: [],
-        searchString: ''
+  componentWillReceiveProps = (nextProps) => {
+    console.log('receiving next movie props: ', nextProps);
+    if (this.props.profile !== nextProps.profile) {
+      this.props.getMoviesInfo(nextProps.profile.films);
     }
+  }
 
-    constructor() {
-        super()
-        this.getMovies()
-    }
+  render() {
+    const { isLoading, movies, error } = this.props;
 
-    getMovies = () => {
-        client.getEntries({
-            content_type: 'movie',
-            query: this.state.searchString
-        })
-        .then((response) => {
-            this.setState({movies: response.items})
-            console.log(this.state.movies)
-        })
-        .catch((error) => {
-          console.log("Error occurred while fetching Entries")
-          console.error(error)
-        })
-    }
+    return (
+      <div>
+        {error ? <p>{error.message}</p> : null}
 
-    onSearchInputChange = (event) => {
-        console.log("Search changed ..." + event.target.value)
-        if (event.target.value) {
-            this.setState({searchString: event.target.value})
-        } else {
-            this.setState({searchString: ''})
-        }
-        this.getMovies()
-    }
-
-    render() {
-        return (
-            <div>
-                { this.state.movies ? (
-                    <div>
-
-                      
-                        <TextField style={{padding: 24}}
-                            id="searchInput"
-                            placeholder="Search for Movies"   
-                            margin="normal"
-                            onChange={this.onSearchInputChange}
-                            />
-                        <Grid container spacing={24} style={{padding: 24}}>
-                            { this.state.movies.map(currentMovie => (
-                                <Grid item xs={12} sm={6} lg={4} xl={3}>
-                                    <Movie movie={currentMovie} />
-                                </Grid>
-                            ))}
-                        </Grid>
-                    </div>
-                ) : "No movies found" }
-            </div>
-        )
-    }
+        { !isLoading ? (
+          <div>
+             <p> I'm gonna show you some movies! </p>
+            <Grid container spacing={24} style={{padding: 24}}>
+              { movies.map(currentMovie => (
+                <Grid item xs={12} sm={6} lg={4} xl={3}>
+                  <Movie movie={currentMovie} />
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+        ) : (
+          <h3>Loading...</h3>
+        )}
+      </div>
+    )
+  }
 }
+
 export default MoviesList;
