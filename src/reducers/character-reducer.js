f07eba55d323914bd113824f17a5e05d9a503295
Izzy <izzy@izzys-mbp.home>
2019-04-10 06:14:09 -0400

    Add reducer actions for api calls

--- src/reducers/character-reducer.js
+++ src/reducers/character-reducer.js
@@ -3,33 +3,40 @@ import {
   GET_PROFILE_SUCCESS,
   GET_PROFILE_ERROR,
   REQUEST_PROFILE,
-  UPDATE_URL
+  UPDATE_URL, 
+  UPDATE_PROFILE
 } from '../constants';
 
-export default function characterReducer(state = initialState.character, action) {
+export default function characterReducer(state = initialState, action) {
   switch (action.type) {
-    case GET_PROFILE_SUCCESS:
+    case REQUEST_PROFILE:
       return {
         ...state,
-        isFetching: false,
-        profile: action.profile
+        isFetching: true,
+        error: null,
       }
     case GET_PROFILE_ERROR:
       return {
         ...state,
         isFetching: false,
+        profile: {},
         error: action.error
       }
-    case REQUEST_PROFILE:
+    case GET_PROFILE_SUCCESS:
       return {
         ...state,
-        isFetching: true
+        isFetching: false,
       }
     case UPDATE_URL:
       return {
         ...state,
         url: action.url
       }
+    case UPDATE_PROFILE:
+        return {
+          ...state,
+          profile: action.profile
+        }
     default:
       return state;
   }
--- src/reducers/movies-reducer.js
+++ src/reducers/movies-reducer.js
@@ -1,14 +1,42 @@
 import initialState from '../store/initialState';
 import {
-  UPDATE_MOVIES,
+  REQUEST_MOVIES,
+  GET_MOVIE_FAILURE, 
+  GET_MOVIE_SUCCESS,
+  UPDATE_MOVIE_DATA,
+  UPDATE_FETCHING
 } from '../constants';
 
 export default function moviesReducer(state = initialState.movies, action) {
   switch (action.type) {
-    case UPDATE_MOVIES:
+    case REQUEST_MOVIES:
+      console.log('MOVIE STATE: ', state)
       return {
         ...state,
-        movies: action.movies
+        isFetching: true,
+        movies: [],
+        error: null
+      }
+    case GET_MOVIE_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      }
+    case GET_MOVIE_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+      }
+    case UPDATE_MOVIE_DATA:
+      return {
+        ...state,
+        movies: [...state.movies, action.movie]
+      }
+    case UPDATE_FETCHING:
+      return {
+        ...state,
+        isFetching: action.isFetching
       }
     default:
       return state;
